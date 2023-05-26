437d798fc22d14bd4edef9d14334d29a152c2c75
fer-ochoa1986 <128333283+fer-ochoa1986@users.noreply.github.com>
2023-05-26 18:28:11 -0300

    Update caporo.js

--- caporo.js
+++ caporo.js
@@ -11,25 +11,26 @@ menubtn.addEventListener("click",(Event) => {
     body.classList.toggle("no-scroll");
 });
 });
-/*validar datos */
-/*function validarFormulario() { }
-
-var nombre = document.forms["formulario"]["nombre"].value;
-var email = document.forms["formulario"]["email"].value;
-var mensaje = document.forms["formulario"]["mensaje"].value;
-if (nombre == "") {alert(" Ingrese su nombre.")};
-return false;
-
-/*if (email == "") {alert("Ingrese su correo electrónico.")};
-return false;*/
-
-/*if (mensaje == "") {alert("¿Que opinión tiene acerca de nuestro producto?")};
-return false;
-
-$(document).ready(function(){
-    $('.menu-icon').click(function(){
-      $('.menu-icon').toggleClass('active');
-      $('.menu').slideToggle(300);
-    });
-  });
-*/
+/*animacion*/
+      document.addEventListener("DOMContentLoaded", function() {
+             var element = document.querySelector(".animate__heartBeat");
+              element.classList.add("animate__animated")
+              });
+/* API sql de precios y stock*/  
+  
+async function fetchData() {
+    try {
+      const response = await fetch("http://localhost/phpmyadmin/index.php?route=/database/structure&db=caporo");
+      if (!response.ok) {
+        throw new Error("Error en la solicitud: " + response.status);
+      }
+      const data = await response.json();
+      // Hacer algo con los datos recibidos
+      console.log(data);
+    } catch (error) {
+      // Manejar cualquier error ocurrido durante la solicitud
+      console.error("Error:", error);
+    }
+  }
+  
+  fetchData();
